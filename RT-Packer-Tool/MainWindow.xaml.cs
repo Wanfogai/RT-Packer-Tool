75a2110d68a20ff652e09233e181aa3ba8509ba3
Wanfogai <81505429+Wanfogai@users.noreply.github.com>
2023-09-03 15:28:11 +0300

    Добавлена загрузка Пака

--- RT-Packer-Tool/MainWindow.xaml.cs
+++ RT-Packer-Tool/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +24,85 @@ namespace RT_Packer_Tool
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string PackPath ;
         public MainWindow()
         {
             InitializeComponent();
+            if (!Directory.Exists(@"Temp"))
+                new DirectoryInfo(@"Temp").Create();
+            else
+                ClearTemp();
+        }
+
+        private void MOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.DefaultExt = ".nw";
+            fileDialog.Filter = "Пак файл (*.nw)|*.nw";
+            if (fileDialog.ShowDialog() == true)
+            {
+                if (fileDialog.FileName.EndsWith(".nw"))
+                {
+                    PackPath = fileDialog.FileName;
+                    LoadPack();
+                }
+                else 
+                {
+                    MessageBox.Show("Формат файла не верен выберете файл .nw");
+                }
+            }
+        }
+
+        public void ClearTemp() 
+        {
+            foreach (FileInfo file in new DirectoryInfo(@"Temp/").GetFiles())
+            {
+                file.Delete();
+            }
+        }
+
+        public void LoadPack() 
+        {
+            ImageBox.Items.Clear();
+            SoundBox.Items.Clear();
+            using (ZipArchive archive = ZipFile.OpenRead(PackPath))
+            {
+                foreach (ZipArchiveEntry entry in archive.Entries)
+                {
+                    if (entry.Name.EndsWith(".png"))
+                    {
+                        ImageBox.Items.Add(entry.Name);
+                    }
+                    if (entry.Name.EndsWith(".ogg"))
+                    {
+                        SoundBox.Items.Add(entry.Name);
+                    }
+                }
+            }
+        }
+
+        public void ChangeImage() 
+        {
+            using (ZipArchive archive = ZipFile.OpenRead(PackPath))
+            {
+                foreach (ZipArchiveEntry entry in archive.Entries)
+                {
+                    if (entry.Name == ImageBox.SelectedItem.ToString())
+                    {
+                        if (!File.Exists($@"Temp/{entry.Name}"))
+                        {
+                            entry.ExtractToFile($@"Temp/{entry.Name}");
+                        }
+                        ImageSourceConverter converter = new ImageSourceConverter();
+                        Sprite.SetValue(Image.SourceProperty, converter.ConvertFromString($@"Temp/{entry.Name}"));
+                    }
+                }
+            }
+        }
+
+        private void ImageBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ChangeImage();
         }
     }
 }
